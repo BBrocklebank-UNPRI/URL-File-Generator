3b61cfc3653fde332479e54769a649b448eee52a
Benjamin Brocklebank <benjamin.brocklebank@unpri.org>
2023-07-28 12:39:52 +0100

    Feat: Separetd script into functions, improved filename/URL validity checks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests, re, os
 from openpyxl import load_workbook
 from requests.exceptions import RequestException
 
+# Define file to scan
 data_file = 'data/URLs_without_files.xlsx'
 
 # Credential to prevnt 406 error
@@ -9,61 +10,65 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; PPC Mac OS X 10_8_7 rv:5.0; en-US) AppleWebKit/533.31.5 (KHTML, like Gecko) Version/4.0 Safari/533.31.5',
 }
 
-# Load the entire workbook.
+# Load the entire workbook
 wb = load_workbook(data_file, data_only=True)
 
-# Load specific worksheet.
+# Load specific worksheet
 ws = wb['0kbUrls']
 
 # Store URLs
 urls = []
-UnamedCount = 0
 
 # Filter values, add to list
 for row in ws:
     url = row[3].value
-    testList = ['https:', 'www.', '.pdf']
+    testList = ['https:', 'www.', '.pdf', '.com', '.co.uk']
     if any(testString in url for testString in testList):
         urls.append(url)
 
-# Download PDF
 
-for url in urls:
 
-##url = 'https://www.bancobpi.pt/contentservice/getContent?documentName=PR_WCS01_UCM01169321'
+def nameFile (url):
+    """Validate file names using URL"""
 
-    if "https://" not in url :
-        url = f"https://{url}"
+    fname = url.rsplit('/', 1)[1]
 
-    try:
-        with requests.get(url, allow_redirects=True, headers=headers) as r:
-           
-            fname = url.rsplit('/', 1)[1]
+    if len(fname) == 0:
+        fname = url
 
-            if len(fname) == 0:
-                UnamedCount + 1
-                fname = f"Unamed {UnamedCount}"
+    if ".pdf" not in fname :
+            fname = f"{fname}.html"
+            print(f"Split: {fname}")
+            #convert to PDF
+
+    invalid = '<>:"/\|?* '
+    for char in invalid:
+        fname = fname.replace(char, '')
+
+    if len(fname) > 256:
+        fname = fname[:250]
+    
+    return fname
+
+def download():
+    """Validate URLs/Download"""
+
+    for url in urls:
+
+        if "https://" not in url :
+            url = f"https://{url}"
+
+        try:
+            with requests.get(url, allow_redirects=True, headers=headers) as r:
             
-            if ".pdf" not in fname :
-                    fname = f"{fname}.html"
-                    print(f"Split: {fname}")
-                    #convert to PDF 
-            
-            invalid = '<>:"/\|?* '
-            for char in invalid:
-                fname = fname.replace(char, '')
+                fname = nameFile(r.url)
 
-            if len(fname) > 256:
-                fname = fname[:250]
-
-            f = open(fname, 'wb')
-            f.write(r.content)
-            f.close
+                f = open(fname, 'wb')
+                f.write(r.content)
+                f.close
 
 
-    except RequestException as e:
-        print(e)
+        except RequestException as e:
+            print(e)
 
-# Loop through urls
-# Save webpage as pdf where no .pdf extension found
-
+download()
