36806040e76db7189ae6ad8c27398be9869b2dc8
Benjamin Brocklebank <benjamin.brocklebank@unpri.org>
2023-08-02 19:46:32 +0100

    Feat/Refac: Added logging, increased script performance

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import requests, re, os, convert
+import requests, re, os, convert, logging
 from openpyxl import load_workbook
 from requests.exceptions import RequestException
 from convert import PdfGenerator
 
-# Define file to scan
+logging.basicConfig(level=logging.WARNING)
+
+# Define Excel file to scan
 data_file = 'data/URLs_without_files.xlsx'
 
 # Credential to prevnt 406 error
@@ -18,41 +20,36 @@ wb = load_workbook(data_file, data_only=True)
 ws = wb['0kbUrls']
 
 # Store URLs
-urls = []
+urls = set()
 
 def fetchURLs (urls):
     """
-    Loop Excel file, add to list
+    Loop Excel column, add to set
     """
     for row in ws:
         url = row[3].value
-        #testList = ['https:', 'www.', '.pdf', '.com', '.co.uk']
-        #if any(testString in url for testString in testList):
-        urls.append(url)
-    urls = list( dict.fromkeys(urls) )
-    print(urls)
+        testList = ['https:', 'www.', '.pdf', '.com', '.co.uk']
+        if any(testString in url for testString in testList):
+            urls.add(url)
+
     downloadPdf(urls)
 
 
 def nameFile (url):
     """
-    Validate file names/Format HTML
+    Filter invalid filename characters
     """
-    pdf_file = []
+    #pdf_file = []
 
-    print(f"First URL: {url}")
-    fname = url.rsplit('/', 1)[1]
-    print(f"Split Name: {fname}")
+    #fname = url.rsplit('/', 1)[1]
+    fname = url
+    #if len(fname) == 0:
+       #fname = url
 
-    if len(fname) == 0:
-        fname = url
-        print(f"Zero Name: {fname}")
-
-    if ".pdf" not in url :
-            fname = f"{fname}.pdf"
-            #print(f"PDF Extension Added: {fname}")
+    #if ".pdf" not in url :
+            #fname = f"{fname}.pdf"
             #Convert to PDF
-            pdf_file = PdfGenerator([url]).main()
+            #pdf_file = PdfGenerator([url]).main()
 
     invalid = '<>:"/\|?* '
     for char in invalid:
@@ -61,42 +58,44 @@ def nameFile (url):
     if len(fname) > 256:
         fname = fname[:250]
 
-    if not pdf_file :
-        return fname, None
+    #if not pdf_file :
+        #return fname, None
     
-    else :
-        return fname, pdf_file[0]
+    #else :
+        #return fname, pdf_file[0]
+    return fname
 
 
 def downloadPdf(urls):
     """
-    Loop through Urls/Save PDFs
+    Loop URLs, check content type, save
     """
 
     for url in urls:
-
+        print(url)
         if "https://" not in url :
             url = f"https://{url}"
 
         try:
             with requests.get(url, allow_redirects=True, headers=headers) as r:
+                contentType = r.headers.get('content-type')
+                print(contentType)
             
-                fname_PDF = nameFile(r.url)
-                print(f"fname_PDF:{fname_PDF}")
+                fname = nameFile(r.url)
 
-                if fname_PDF[1] == None : #logic error, check for second value
-                    print(f"No converted PDF:{fname_PDF}")
-                    f = open(fname_PDF[0], 'wb')
+                if 'application/pdf' in contentType:
+                    f = open(f"{fname}.pdf", 'wb')
                     f.write(r.content)
                     f.close
-                
-                else :
-                    print(f"Converted PDF present:{fname_PDF}")
-                    with open(fname_PDF[0], "wb") as outfile:
-                        outfile.write(fname_PDF[1].getbuffer())
 
+                elif 'text/html' in contentType:
+                    pdf_file = PdfGenerator([url]).main()
+                    with open(f"{fname}.pdf", "wb") as outfile:
+                        outfile.write(pdf_file[0].getbuffer())
+
+            logging.warning(f"Organisation Name: URL: {url} Result: Success")
 
         except RequestException as e:
-            print(e)
+            logging.warning(f" Organisation Name: URL: {url} Result: Failed")
 
 fetchURLs(urls)
